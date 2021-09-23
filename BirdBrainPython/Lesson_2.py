01eba7a237619109caad9b0fee429d608880e04f
Lorenzo Gladney Jr <zo@Lorenzos-MBP.attlocal.net>
2021-09-22 18:08:10 -0700

    first comit

---
+++ BirdBrainPython/Lesson_2.py
@@ -0,0 +1,27 @@
+from BirdBrain import Finch
+bird=Finch()
+#addition = +, subtraction = -, multiplication = *, division = /,Modulo = %,
+distance = bird.getDistance()
+rightLight = bird.getLight('R')
+leftLight = bird.getLight('L')
+buttonA = bird.getButton('A')
+orientation = bird.getOrientation()
+rightEncode = bird.getEncoder('R')
+diffrence = rightLight - leftLight
+mean = rightLight + leftLight / 2
+print("mean: ", mean)
+"""
+print("distance: ", distance)
+print("rightlight: ", rightLight)
+print("leftlight: ", leftLight)
+print("ButtonA: ", buttonA)
+print("Orientation: ", orientation)
+print("RightWheel: ", rightEncode)
+
+print("distance: ",type(distance))
+print("rightlight: ",type(rightLight))
+print("leftlight: ", type(leftLight))
+print("ButtonA: ", type(buttonA))
+print("Orientation: ", type(orientation))
+print("RightWheel: ", type(rightEncode))
+"""
---
+++ BirdBrainPython/Lesson_3.py
@@ -0,0 +1,9 @@
+from BirdBrain import Finch
+from time import sleep
+bird=Finch()
+
+
+bird.setTail(1,0,0,100)
+bird.setTail(4,0,0,100)
+sleep(2)
+bird.stopAll()
