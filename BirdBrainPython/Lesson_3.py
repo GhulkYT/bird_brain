14363f69f330c96ba927d176c14f5ed08aab69c2
Lorenzo Gladney Jr <zo@Lorenzos-MBP.attlocal.net>
2021-10-11 17:29:14 -0700

    Pushing so baba can help

--- BirdBrainPython/Lesson_3.py
+++ BirdBrainPython/Lesson_3.py
@@ -2,8 +2,22 @@ from BirdBrain import Finch
 from time import sleep
 bird=Finch()
 
-
+"""
+exercise 3
 bird.setTail(1,0,0,100)
 bird.setTail(3,0,0,100)
 sleep(2)
+bird.stopAll()
+"""
+r = 100
+b = 0
+g = 0
+for x in range(3):
+    bird.setBeak(all,r,b,g)
+    bird.setTail(all,r,b,g)
+    sleep(2)
+    b = r * 1
+    r = r * 0
+    g = g + 50
+
 bird.stopAll()
