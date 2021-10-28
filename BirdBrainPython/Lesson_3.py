c83679bb3c234a029059f7f4a2d46eee3e4af6ec
Lorenzo Gladney Jr <zo@Lorenzos-MBP.attlocal.net>
2021-10-27 23:57:42 -0700

    finished lesson 3

--- BirdBrainPython/Lesson_3.py
+++ BirdBrainPython/Lesson_3.py
@@ -21,5 +21,42 @@ for x in range(3):
     b = r * 1
     r = r * 0
 """
+""" 
+exercise 5 
+userResponse = input("Which tail light (1-4) should be red? ") # Get user input 
+number = int(userResponse) # Convert to int
+bird.setTail(number , 100,0,0)
+sleep(1) 
+bird.stopALL() 
+it asked me what number of the tail would light up and the number i put changed. 
+""" 
+""" 
+exercise 6 
+redLight = input("What should the amount of red be 0-100? ") # Get user input 
+greenLight = input("What should the amount of green be 0-100? ") # Get user input 
+blueLight = input("What should the amount of blue be 0-100? ") # Get user input 
+r = int(redLight) # Convert to int 
+g = int(greenLight) # Convert to int 
+b = int(blueLight) # Convert to int 
+bird.setTail(all,r,g,b) 
+bird.setBeak(r,g,b) 
+sleep(3) 
+bird.stopALL() 
+""" 
+""" 
+exercise 7 
+r = 100 
+b = 0 
+g = 0 
+for x in range(4): 
+bird.setBeak(r,g,b) 
+bird.setTail(all,r,g,b) 
+g = b 
+b = r * 1 
+r = r * 0 
+bird.setMove('f',5,100) 
+bird.setTurn('R',90,50) 
+"""
+
 
 bird.stopAll()
